76d151b0ac92bd88a7b0e87f9ab5142670c3b7cd
Rubicell <rubicell741@gmail.com>
2025-06-20 00:26:41 +0900

    added idf.ts

---
+++ src/idf.ts
@@ -0,0 +1,21 @@
+class IDFClass {
+  constructor(name: string) {}
+}
+
+class IDFObject {
+}
+
+export class IDF {
+  constructor() {
+  }
+  
+  toString(indentSize: number, fieldSize: number) {
+    for (let obj of self.objects)
+    return "";
+  }
+}
+
+
+function readIDF(idfString: string, idd: string) { }
+
+function writeIDF(epts: any) { }
