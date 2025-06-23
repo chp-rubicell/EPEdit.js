36cac33ea5f03151ae74ce22dea05acea9cbd60e
Rubicell <rubicell741@gmail.com>
2025-06-23 16:07:23 +0900

    changed git crlf behavior, started working on toString()

--- src/idf.ts
+++ src/idf.ts
@@ -1,21 +1,28 @@
+//! TEMP
+const idd: Record<string, string> = {
+  "timestep": "Timestep",
+  "buildingsurface:detailed": "BuildingSurface:Detailed"
+}
+
 // interface IDFObject {
 //   name: string;
 //   [key: string]: number | string;
 // }
-type IDFObject = Record<string, number | string>;
+type IDFObject = Record<string, string | number | boolean>;
 
 class IDFClass {
   name: string;
   idfObjects: IDFObject[];
 
   constructor(className: string) {
-    this.name = className;
+    this.name = className.toLowerCase();
     this.idfObjects = [];
   }
 }
 
 export class IDF {
-  // Hyperparameters
+  //* Hyperparameters
+  // whether to check validity when modifying the IDF object
   CHECKVALID: boolean = false;
   // contains IDFClasses
   objects: Record<string, IDFClass>;
@@ -24,18 +31,43 @@ export class IDF {
     this.objects = {};
   }
 
-  addObject(className: string, fields: string) {
-    if (!(className in this.objects)) {
-      this.objects[className] = new IDFClass(className);
+  /**
+   * Returns an IDFClass object with the corresponding className
+   * @param className IDF class name (case insensitive)
+   * @returns IDFClass
+   */
+  private getIDFClass(className: string): IDFClass {
+    const classNameLower: string = className.toLowerCase();
+    if (!(classNameLower in this.objects)) {
+      // 만약 해당 class가 한 번도 한 생겼다면
+      this.objects[classNameLower] = new IDFClass(classNameLower);
     }
-    this.objects[className].idfObjects.push( {fields: 0} );
-    console.log(this.objects[className]);
-    console.log(className in this.objects);
+    return this.objects[classNameLower];
   }
 
-  toString(indentSize: number, fieldSize: number) {
-    // for (let obj of self.objects) { }
-    // return "";
+  addObject(className: string, fields: IDFObject) {
+    this.getIDFClass(className).idfObjects.push(fields);
+  }
+
+  toString(classIndentSize: number = 2, fieldIndentSize: number = 4, fieldSize: number = 22): string {
+    const classIndent = " ".repeat(classIndentSize);
+    const fieldIndent = " ".repeat(fieldIndentSize);
+
+    let outputString = "";
+
+    for (const [classNameLower, idfClass] of Object.entries(this.objects)) {
+      outputString += `${classIndent}${idd[classNameLower]}\n`;
+      for (const idfObject of idfClass.idfObjects) {
+        // Object.entries(idfObject).forEach(([fieldName, fieldVal], fieldIndex) => {});
+        for (const [fieldName, fieldVal] of Object.entries(idfObject)) {
+          const fieldPaddingLength = fieldSize - String(fieldVal).length;
+          const fieldPadding = " ".repeat(fieldPaddingLength >= 0 ? fieldPaddingLength : 0);
+          outputString += `${fieldIndent}${fieldPadding}${fieldVal},  !- ${fieldName}\n`;
+        }
+      }
+    }
+
+    return outputString;
   }
 }
 
@@ -45,12 +77,11 @@ function readIDF(idfString: string, idd: string) { }
 function writeIDF(epts: any) { }
 
 
-let test = new IDF();
+let idf = new IDF();
 
-console.log("started");
+idf.addObject("Timestep", { "Number of Timesteps per Hour": 4 });
 
-test.addObject("test", "f1");
-test.addObject("test", "f1");
-test.addObject("test", "f2");
 
-console.log(test.CHECKVALID)
+// console.log(idf.objects["Timestep"]);
+
+console.log(idf.toString())
