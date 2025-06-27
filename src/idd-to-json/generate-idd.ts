5ddab465d61ccbb66acd2e862e0f2718c0d0b35b
Rubicell <chp.rubicell@gmail.com>
2025-06-27 16:38:13 +0900

    first draft of idd-to-json generator.

--- src/idd-to-json/generate-idd.ts
+++ src/idd-to-json/generate-idd.ts
@@ -20,22 +20,35 @@ for (const key of Object.keys(schema.properties)) {
 }
 */
 
+// for reading & using in program
 interface classProps {
   className: string;
   fieldNames: Record<string, string>;
 }
-type classFields = Record<string, classProps>;
+// for export
+interface classPropsExport {
+  className: string;
+  fields: string[];
+}
+// type classFields = Record<string, classProps>;
+type classFieldsExport = Record<string, classPropsExport>;
 
-export const fieldDictionary: classFields = Object.fromEntries(
+export const fieldDictionary: classFieldsExport = Object.fromEntries(
   Object.entries(schema.properties).map(([className, propData]: [string, any]) => {
     const props: any = propData;
     const fieldNames: string[] = Object.values(props.legacy_idd.field_info).map((field_info: any) => field_info.field_name);
+    /*
     const fieldKeyNameDict = Object.fromEntries(
       Object.values(fieldNames).map((fieldName) => [fieldNameToKey(fieldName, true), fieldName])
     )
     const classProps: classProps = {
       className: className,
       fieldNames: fieldKeyNameDict
+    }
+    */
+    const classProps: classPropsExport = {
+      className: className,
+      fields: fieldNames
     }
     return [className.toLowerCase(), classProps];
   })
@@ -57,17 +70,19 @@ import { promises as fs } from 'fs';
 // Create an async function to handle the export
 async function exportRecordToJsonFile(
   data: Record<string, any>,
-  fileName: string,
+  filePath: string,
 ) {
   try {
     //? Serialize the object to a JSON string
     // The 'null, 2' arguments pretty-print the JSON with 2-space indentation
-    const jsonString = JSON.stringify(data, null, 2);
+    // const jsonString = JSON.stringify(data, null, 2);
+
+    // Minified output
+    const jsonString = JSON.stringify(data);
 
     //? Define the output file path
     // path.join ensures the path is correct for any operating system
     // const filePath = path.join(__dirname, fileName);
-    const filePath = `./${fileName}.json`;
 
     //? Write the string to a file
     await fs.writeFile(filePath, jsonString, "utf-8");
@@ -78,4 +93,4 @@ async function exportRecordToJsonFile(
   }
 }
 
-exportRecordToJsonFile(fieldDictionary, 'test');
+exportRecordToJsonFile(fieldDictionary, './src/idd-to-json/jsons/test-mini.json');
